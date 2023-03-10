63ea7a114189275c5d4b68ad646dae35f63fdf3a
Dark13Night <kitty7.dec.2004@gmail.com>
2023-03-09 23:12:54 -0500

    added comment

--- Hello world.cpp
+++ Hello world.cpp
@@ -3,6 +3,6 @@ include <string>
 int main() {
 std::string name;
 std::cout << "Please enter name";
-std::cin >> name;
+std::cin >> name;   //вводим имя
 std::cout << "Hello world from " << name;
 }
